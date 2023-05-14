78abc07c8636fa3d259938ca4273a1319bf42b88
[Diogo Júnior] <diogofbjr@gmail.com]>
2023-05-14 17:34:44 -0300

    update

--- src/components/Home/FirstSessionLP/styles.ts
+++ src/components/Home/FirstSessionLP/styles.ts
@@ -6,12 +6,15 @@ export const FirstComponent = styled.div`
     background-size: contain;
     background-repeat: no-repeat;
     height: auto;
+    margin-top: 3em;
     .text{
         h1{
             color: #F5F5F5;
             font-family: 'Montserrat';
             font-style: normal;
             font-weight: 400;
+            margin-top: 1em;
+            margin-left: 1em;
         }
     }
     .buttons{
@@ -19,84 +22,76 @@ export const FirstComponent = styled.div`
             border: none;
         }
         .firstButton{
+            background: none;
             border: 1px solid #2975CC;
             color: #2975CC;
+            padding: 5px 10px;
+            margin-top: 3.4em;
         }
         .secondButton{
             background: #2975CC;
             color: #FAFAFA;
             cursor: pointer;
             transition: all 0.4s ease-in-out;
+            padding: 6px 11px;
+            margin-top: 3.4em;
         }
     }
-    @media screen and (min-width: 360px) and (max-width: 559px) {
+    @media screen and (min-width: 360px) and (max-width: 459px) {
+        background-size: 550px auto !important;
+        .text{
+            h1{
+                font-size: 27px;
+                width: 10em;
+            }
+        }
+        .buttons{
+            margin-bottom: 9em;
+            button{
+                border-radius: 5px;
+                font-size: 13px;
+            }
+            .firstButton{
+                margin-left: 1em;
+            }
+            .secondButton{
+                margin-left: 0.5em;
+            }
+            .secondButton:hover{
+                transform: scale(1.2);
+                margin-left: 1.3em;
+            } 
+        }
+    }
+    @media screen and (min-width: 460px) and (max-width:559px) {
         background-size: 650px auto !important;
-        margin-top: 3em;
         .text{
             h1{
-                font-size: 20px;
+                font-size: 37px;
                 width: 10em;
-                margin-top: 1em;
-                margin-left: 1em;
             }
         }
         .buttons{
             button{
                 border-radius: 5px;
-                font-size: 13px;
+                font-size: 17px;
             }
             .firstButton{
                 background: none;
                 margin-left: 1em;
-                margin-top: 3.4em;
-                padding: 5px 10px;
+                
+                
             }
             .secondButton{
                 margin-left: 0.5em;
                 margin-top: 3.4em;
-                padding: 6px 11px;
             }
             .secondButton:hover{
-                transform: scale(1.2);
+                transform: scale(1.1);
                 margin-left: 1.3em;
             }
             margin-bottom: 9em;
         }
-    }
-    @media screen and (min-width: 560px) and (max-width:759px) {
-        background-size: 500px auto !important;
-        margin-top: 3em;
-        .text{
-            h1{
-                font-size: 20px;
-                width: 10em;
-                margin-top: 1em;
-                margin-left: 1em;
-            }
-        }
-        .buttons{
-            button{
-                border-radius: 5px;
-                font-size: 13px;
-            }
-            .firstButton{
-                background: none;
-                margin-left: 1em;
-                margin-top: 3.4em;
-                padding: 5px 10px;
-            }
-            .secondButton{
-                margin-left: 0.5em;
-                margin-top: 3.4em;
-                padding: 6px 11px;
-            }
-            .secondButton:hover{
-                transform: scale(1.2);
-                margin-left: 1.3em;
-            }
-            margin-bottom: 9em;
-        }
-
     }
     
 `
@@ -108,18 +103,29 @@ export const FirstComponent = styled.div`
 
 
 
-export const Text = styled.h1`
-    font-size: 16px;
+export const Text = styled.p`
     color: #F5F5F5;
     text-align: justify;
     font-family: 'Montserrat';
     font-style: normal;
     font-weight: 400;
     margin-top: 1em;
-    margin-left: 3em;
-    
     line-height: 20px;
     letter-spacing: 0.5px;
+    @media screen and (min-width: 360px) and (max-width: 459px) {
+        font-size: 16px ;
+        margin-left: 1.7em;
+        width: 11.6em;
+        
+    }
+    @media screen and (min-width: 460px) and (max-width:559px) {
+        font-size: 19px ;
+        margin-left: 2.1em;
+        width: 11.8em;
+    }
+    
+    
+    
 
     @media (min-width: 360px) {
         &::before {
@@ -128,7 +134,7 @@ export const Text = styled.h1`
     }
 
     @media (max-width: 559px) {
-        width: 14em !important;
+        
         &::before {
             content: "Ajudamos você a construir uma presença online forte e profissional que reflita o futuro do seu negócio!";
         }
